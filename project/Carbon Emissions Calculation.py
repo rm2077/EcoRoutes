9ab6428cb2b41402ee63e2416c50f8f228e2037d
Raghuram-gandham <138952310+Raghuram-gandham@users.noreply.github.com>
2023-09-17 00:10:52 -0400

    Update Carbon Emissions Calculation.py

--- project/Carbon Emissions Calculation.py
+++ project/Carbon Emissions Calculation.py
@@ -1,13 +1,13 @@
 # Calculate carbon emissions
 def calculate_carbon_emissions():
     #initial values
-    Distance_Traveled = float(input("Enter the total number of miles driven: "))
+    Distance_Traveling = float(input("Enter the total number of miles driving: "))
     Fuel_Usage = float(input("Enter the carbon content of the fuel in grams per gallon: "))
     Density = float(input("Enter the density of the fuel used: "))
     Heat_Values = float(input("Enter the heat value of the fuel used: "))
     
     # Calculate carbon emissions
-    Travel_Carbon_Footprint = ((Distance_Traveled + Fuel_Usage) * 2.3 * Heat_Values * Density)
+    Travel_Carbon_Footprint = ((Distance_Traveling + Fuel_Usage) * 2.3 * Heat_Values * Density)
 
     # Print the result
     print(f"The estimated carbon emissions are {Travel_Carbon_Footprint:.2f} grams.")
