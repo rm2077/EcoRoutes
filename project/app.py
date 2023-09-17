4b2764eec8986a209d54d281ab453ef3747364b9
Raunak Maheshwari <rauna@macbook-air.mynetworksettings.com>
2023-09-16 23:44:06 -0400

    Added map

--- project/app.py
+++ project/app.py
@@ -2,12 +2,17 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/", methods=['GET'])
 def index():
     return render_template("index.html")
 
-def open_map():
-    pass
+@app.route("/route", methods=['GET'])
+def route():
+    return render_template("routes.html")
+
+@app.route("/carbon", methods=['GET'])
+def carbon():
+    return render_template("carbon.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
