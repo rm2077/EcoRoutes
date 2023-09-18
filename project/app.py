aecc9c896c7c6c73871f94fdec0565a9b6ddf894
Raunak Maheshwari <rauna@macbook-air.mynetworksettings.com>
2023-09-17 20:12:09 -0400

    Added frontend/backend code

--- project/app.py
+++ project/app.py
@@ -10,9 +10,18 @@ def index():
 def route():
     return render_template("routes.html")
 
-@app.route("/carbon", methods=['GET'])
+@app.route("/carbon", methods=['GET', 'POST'])
 def carbon():
-    return render_template("carbon.html")
+    if request == "POST":
+        car_brand = request.form.get('Car Brand: ')
+        car_type = request.form.get('Car Type: ')
+        miles_driven = float(request.form.get('miles_driven'))
+        fuel_per_gallon = float(request.form.get('fuel_per_gallon'))
+        return render_template("carbon.html", car_brand=car_brand, car_type = car_type)
+
+
+    else:
+        return render_template("carbon.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
