9af4d538f0985d9133c9c3f83deb748efd1dab4c
Raunak Maheshwari <rauna@macbook-air.mynetworksettings.com>
2023-09-18 23:18:07 -0400

    Added code

--- project/app.py
+++ project/app.py
@@ -12,16 +12,15 @@ def route():
 
 @app.route("/carbon", methods=['GET', 'POST'])
 def carbon():
-    if request == "POST":
-        car_brand = request.form.get('Car Brand: ')
-        car_type = request.form.get('Car Type: ')
-        miles_driven = float(request.form.get('miles_driven'))
-        fuel_per_gallon = float(request.form.get('fuel_per_gallon'))
-        return render_template("carbon.html", car_brand=car_brand, car_type = car_type)
+    if request.method == "POST":
+        car_brand = request.form.get('carBrand')
+        car_type = request.form.get('car_type')
+        miles = request.form.get('miles')
+        fuel = request.form.get('fuel')
+        
+        return render_template("success.html", car_brand=car_brand, car_type=car_type, miles=miles, fuel=fuel)
 
-
-    else:
-        return render_template("carbon.html")
+    return render_template('carbon.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
